7e17590a873a9ee9fad62f9475345c0a4115d718
harshita <76101819+HELLOChamp@users.noreply.github.com>
2024-06-15 09:29:28 +0530

    Update sketch.js

--- Project 21/sketch.js
+++ Project 21/sketch.js
@@ -1,69 +1,70 @@
-var canvas;
-var block1,block2,block3,block4;
-var ball, edges;
-var music;
+// Create game area
+const gameArea = document.createElement('div');
+document.body.appendChild(gameArea);
 
-function preload(){
-    // load sound here
-    music = loadSound()
+// Style game area
+gameArea.style.position = 'relative';
+gameArea.style.width = '80vw';
+gameArea.style.height = '80vh';
+gameArea.style.border = '2px solid black';
+gameArea.style.overflow = 'hidden';
+gameArea.style.margin = '0 auto';
+gameArea.style.backgroundColor = '#f0f0f0';
+gameArea.style.display = 'flex';
+gameArea.style.justifyContent = 'center';
+gameArea.style.alignItems = 'center';
+
+// Create the box
+const box = document.createElement('div');
+gameArea.appendChild(box);
+
+// Style the box
+box.style.position = 'absolute';
+box.style.width = '50px';
+box.style.height = '50px';
+box.style.backgroundColor = 'red';
+
+// Initial positions and velocities
+let boxX = 0;
+let boxY = 0;
+let velocityX = 2;
+let velocityY = 2;
+
+// Colors for different sides
+const colors = {
+    left: 'red',
+    right: 'blue',
+    top: 'green',
+    bottom: 'yellow'
+};
+
+function moveBox() {
+    boxX += velocityX;
+    boxY += velocityY;
+
+    // Check for collisions with game area boundaries
+    if (boxX <= 0) {
+        velocityX = Math.abs(velocityX);
+        box.style.backgroundColor = colors.left;
+    } else if (boxX + box.offsetWidth >= gameArea.clientWidth) {
+        velocityX = -Math.abs(velocityX);
+        box.style.backgroundColor = colors.right;
+    }
+
+    if (boxY <= 0) {
+        velocityY = Math.abs(velocityY);
+        box.style.backgroundColor = colors.top;
+    } else if (boxY + box.offsetHeight >= gameArea.clientHeight) {
+        velocityY = -Math.abs(velocityY);
+        box.style.backgroundColor = colors.bottom;
+    }
+
+    // Update box position
+    box.style.left = boxX + 'px';
+    box.style.top = boxY + 'px';
+
+    requestAnimationFrame(moveBox);
 }
 
-
-function setup(){
-    canvas = createCanvas(800,600);
-
-    block1 = createSprite(0,580,360,30);
-    block1.shapeColor = "blue";
-
-    block2 = createSprite(295,580,200,30);
-    block2.shapeColor = "orange";
-
-    //create two more blocks i.e. block3 and block4 here
-    block3 = createSprite(490,580,160,30);
-    block3.shapeColor = "red";
-
-    block4 = createSprite(700,580,160,30);
-    block4.shapeColor = "green";
-
-    ball = createSprite(random(20,750),100, 40,40);
-    ball.shapeColor = rgb(255,255,255);
-    //write code to add velocityX and velocityY
-    ball.velocityY = 19;
-    ball.velocityX = 9; 
-}
-
-function draw() {
-    background(rgb(169,169,169));
-    edges=createEdgeSprites();
-    ball.bounceOff(edges);
-
-    
-    //write code to bounce off ball from the block1 
-    if(block1.isTouching(ball) && ball.bounceOff(block1)){
-        ball.shapeColor = "blue";
-        music.play();
-    }
-
-
-
-    if(block2.isTouching(ball)){
-        ball.shapeColor = "orange";
-        //write code to set velocityX and velocityY of ball as 0
-        ball.velocityX = 0;
-        ball.velocityY = 0;
-        //write code to stop music
-        music.stop();
-    }
-
-    //write code to bounce off ball from the block3
-    if(block3.isTouching(ball) && ball.bounceOff(block3)){
-        ball.shapeColor = "red";
-    }
-
-    //write code to bounce off ball from the block4
-    if(block4.isTouching(ball) && ball.bounceOff(block4)){
-        ball.shapeColor = "green";
-    }
-
-    drawSprites();
-}
+// Start the animation
+moveBox();
